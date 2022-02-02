7fbdebe7ac573569082136e7539e99f9205f7b84
Olena Honcharuk <gasmin07@gmail.com>
2022-02-02 19:23:51 +0200

    feat: get word and page

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,27 @@ import './style.css';
 import './css/normalise.css';
 import { navigation } from './js/router';
 import { getWord, getWords } from './js/api';
+import { Word} from './js/types';
 
 console.log('App is running');
 
 navigation();
 
 async function words(page: number) {
-  const items = await getWords(page);
-  // console.log(`items are ${items}`);
+  const items = await getWords(page).then((data: Word[]) =>{
+    console.log(data);
+
+  });
 }
 words(1);
 
 
 
 async function word(id: string) {
-  const word = await getWord(id);
-  // console.log(`item is ${word}`);
+  const word = await getWord(id).then((data: Word) =>{
+    console.log(`item is ${data.word}`);
+    console.log(data);
+
+  });
 }
 word('5e9f5ee35eb9e72bc21af4c9');
