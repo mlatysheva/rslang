bee246d440ec07073e09ca481aebf6259db0bd3e
Yuliya Mironova <rabkevich15@gmail.com>
2022-02-07 14:32:20 +0100

    feat: game with btn 1st page

--- src/game/gameDescribe.ts
+++ src/game/gameDescribe.ts
@@ -12,7 +12,18 @@ export function renderGame(): any {
   gameDescription.classList.add('game-description');
   gameDescription.textContent = 'Цель игры: выбрать правильный перевод услышанного слова.';
   gameName.appendChild(gameDescription);
+  const levelOfDifficult = document.createElement('div');
+  levelOfDifficult.classList.add('levelsGame1');
+  levelOfDifficult.innerHTML = `
+  <button id="levelGame0" class="game1 level1">Level 1</button>
+  <button id="levelGame1" class="game1 level2">Level 2</button>
+  <button id="levelGame2" class="game1 level3">Level 3</button>
+  <button id="levelGame3" class="game1 level4">Level 4</button>
+  <button id="levelGame4" class="game1 level5">Level 5</button>
+  <button id="levelGame5" class="game1 level6">Level 6</button>`;
+
   gameSectionDescribe.appendChild(gameName);
+  gameSectionDescribe.appendChild(levelOfDifficult);
 
   return gameSectionDescribe;
 }
