8e6452acf725a0570323dfb7a8dc8cd2ae64f9f3
Maria-Latysheva <mlatysheva@gmail.com>
2022-02-15 16:50:51 +0100

    fix: correct bug in learnedWords

--- src/statistics/globalStorage.ts
+++ src/statistics/globalStorage.ts
@@ -1,9 +1,19 @@
-export const learnedWords: string[] = [];
+export let learnedWords: string[];
 
-localStorage.setItem('learnedWords', JSON.stringify(learnedWords));
-const wordsfromSS = JSON.parse(localStorage.getItem('learnedWords') as string);
+if (JSON.parse(localStorage.getItem('learnedWords') as string)) {
+  learnedWords = JSON.parse(localStorage.getItem('learnedWords') as string)
+} else {
+  learnedWords = [];
+  localStorage.setItem('learnedWords', JSON.stringify(learnedWords));
+}
 
 // global variable to store ids of difficult words - words that the user marked as such in the text book or
 // did not guess correctly in mini games
-export const difficultWords: string[] = [];
-localStorage.setItem('difficultWords', JSON.stringify(difficultWords));
+export let difficultWords: string[];
+
+if (JSON.parse(localStorage.getItem('difficultWords') as string)) {
+  difficultWords = JSON.parse(localStorage.getItem('difficultWords') as string)
+} else {
+  difficultWords = [];
+  localStorage.setItem('difficultWords', JSON.stringify(difficultWords));
+}
