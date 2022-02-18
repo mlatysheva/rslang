0566fc392ba8c97613eedc7d90b1b51a976d6a21
Maria-Latysheva <mlatysheva@gmail.com>
2022-02-18 21:31:56 +0100

    feat: add adaptive and fix bugs

--- src/login/loginLogout.ts
+++ src/login/loginLogout.ts
@@ -11,13 +11,11 @@ export function logout() {
   const logoutBtn = document.getElementById('signout-link');
   (<HTMLElement>logoutBtn).addEventListener('click', () => {
     if (getItemFromLocalStorage('email') != null) {
-      console.log('logout is clicked');
       localStorage.clear();
       localStorage.removeItem('id');
       localStorage.removeItem('email');
       localStorage.removeItem('token');
       renderUserName();
-      console.log(`window.location.hash is ${window.location.hash}`);
       window.history.back();
       window.location.reload();
     }
